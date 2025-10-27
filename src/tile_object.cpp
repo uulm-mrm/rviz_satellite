9e84b01b9d388f5ddef51731512a9e219bf10244
Florian <florian.hillitzer@uni-ulm.de>
2025-10-27 10:53:10 +0000

    changed texture filtering mode to improve visualization of tiles

--- src/tile_object.cpp
+++ src/tile_object.cpp
@@ -182,7 +182,9 @@ void TileObject::setupMaterial()
   material_->setCullingMode(Ogre::CULL_NONE);
   material_->setDepthWriteEnabled(false);
   auto texture_unit_state = material_->getTechnique(0)->getPass(0)->createTextureUnitState();
-  texture_unit_state->setTextureFiltering(Ogre::TFO_BILINEAR);
+  // Smoothing of tiles visualization
+  texture_unit_state->setTextureFiltering(Ogre::TFO_NONE);  // no bilinear filtering
+  texture_unit_state->setTextureAddressingMode(Ogre::TextureUnitState::TAM_CLAMP); // do not repeat on border
 }
 
 void TileObject::setupSceneNodeWithManualObject()
